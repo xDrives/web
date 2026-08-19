be9378d213b537ec0b28be27ddc7d3040e2d38d8
xDrives <data.recovery.mail.me@gmail.com>
2026-08-19 09:55:02 +0600

    Add files via upload

--- auth-module.js
+++ auth-module.js
@@ -8,7 +8,7 @@
 
 // Replace this URL with your published Google Apps Script Web App URL
 // The script should return JSON: { "databaseURL": "https://..." }
-const MASTER_CONFIG_URL = 'https://script.google.com/macros/s/AKfycbzU8kIjS09CHeGWGkr0VrDCEkAT0myYcZtW4g9O09CPyqFbPOxOrE7YdqzokJhx3813/exec';
+const MASTER_CONFIG_URL = 'https://script.google.com/macros/s/AKfycbxVK0ZzzR3PuzMhVItVcvuec8wlbLnDTA8cDC1DqqaoummiecAyGF51-Jhi2a86I7gm/exec';
 
 class AuthModule {
     constructor() {
